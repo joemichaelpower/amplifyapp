b0473481d69b11c8342ecc7326646a657acb3557
Joe Ajoy <joemichaelpower@gmail.com>
2021-05-30 13:44:17 -0400

    headingt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>This is the new app</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

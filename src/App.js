c63e516dcf414f9f4050719821feb2b8af703087
Joe Ajoy <joemichaelpower@gmail.com>
2021-05-30 13:42:16 -0400

    I will teach you react

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I will teach you react
         </a>
       </header>
     </div>
